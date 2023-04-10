a709df6d855d79c59c8004b60ef4425bf0e7b90e
ithomasdev <ithomasdev@gmail.com>
2023-04-10 19:45:44 -0400

    added new architecture

--- barcode_scanner/App.js
+++ barcode_scanner/App.js
@@ -1,20 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Button, Header, Icon } from "@rneui/base";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Header
+          leftComponent={{
+            icon: 'menu',
+            color: '#fff'
+          }}
+          centerComponent={{
+            text: 'Discount Scan',
+            style: styles.heading
+          }}
+          rightComponent={
+            <View style={styles.headerRight}>
+              <TouchableOpacity>
+                <Icon name="scanner" color="white" />
+              </TouchableOpacity>
+            </View>
+          }
+          backgroundColor='#262A56'
+        />
+        <View style={styles.body}>
+          
+        </View>
+    </SafeAreaView>
+    
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  container: { backgroundColor: '#262A56'},
+  headerContainer: {
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+    marginBottom: 0,
+    width: '100%',
+    paddingVertical: 0
   },
+  navigation: {
+    width: '100%'
+  },
+  heading: {
+    color: 'white',
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  headerRight: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginTop: 5,
+  },
+  body: {
+    paddingTop: 1,
+    backgroundColor: '#f5f5f5',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 });
